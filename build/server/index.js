7c1396935420a49c08fbf51716ae453d52c3c7e0
kieran <kieran6roberts@gmail.com>
2024-12-29 10:08:59 +0000

    chore: init core dev dependencies

--- build/server/index.js
+++ build/server/index.js
@@ -1,13 +1,12 @@
 import { jsx, jsxs } from "react/jsx-runtime";
 import { PassThrough } from "node:stream";
 import { createReadableStreamFromReadable } from "@remix-run/node";
-import { RemixServer, Meta, Links, Outlet, Scripts } from "@remix-run/react";
-import * as isbotModule from "isbot";
+import { RemixServer, Outlet, Meta, Links, ScrollRestoration, Scripts } from "@remix-run/react";
+import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 const ABORT_DELAY = 5e3;
 function handleRequest(request, responseStatusCode, responseHeaders, remixContext, loadContext) {
-  let prohibitOutOfOrderStreaming = isBotRequest(request.headers.get("user-agent")) || remixContext.isSpaMode;
-  return prohibitOutOfOrderStreaming ? handleBotRequest(
+  return isbot(request.headers.get("user-agent") || "") ? handleBotRequest(
     request,
     responseStatusCode,
     responseHeaders,
@@ -18,18 +17,6 @@ function handleRequest(request, responseStatusCode, responseHeaders, remixContex
     responseHeaders,
     remixContext
   );
-}
-function isBotRequest(userAgent) {
-  if (!userAgent) {
-    return false;
-  }
-  if ("isbot" in isbotModule && typeof isbotModule.isbot === "function") {
-    return isbotModule.isbot(userAgent);
-  }
-  if ("default" in isbotModule && typeof isbotModule.default === "function") {
-    return isbotModule.default(userAgent);
-  }
-  return false;
 }
 function handleBotRequest(request, responseStatusCode, responseHeaders, remixContext) {
   return new Promise((resolve, reject) => {
@@ -115,35 +102,47 @@ const entryServer = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineP
   __proto__: null,
   default: handleRequest
 }, Symbol.toStringTag, { value: "Module" }));
-function App() {
-  return /* @__PURE__ */ jsxs("html", { children: [
+const links = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous"
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+  }
+];
+function Layout({ children }) {
+  return /* @__PURE__ */ jsxs("html", { lang: "en", children: [
     /* @__PURE__ */ jsxs("head", { children: [
-      /* @__PURE__ */ jsx(
-        "link",
-        {
-          rel: "icon",
-          href: "data:image/x-icon;base64,AA"
-        }
-      ),
+      /* @__PURE__ */ jsx("meta", { charSet: "utf-8" }),
+      /* @__PURE__ */ jsx("meta", { name: "viewport", content: "width=device-width, initial-scale=1" }),
       /* @__PURE__ */ jsx(Meta, {}),
       /* @__PURE__ */ jsx(Links, {})
     ] }),
     /* @__PURE__ */ jsxs("body", { children: [
-      /* @__PURE__ */ jsx("h1", { children: "Hello world!" }),
-      /* @__PURE__ */ jsx(Outlet, {}),
+      children,
+      /* @__PURE__ */ jsx(ScrollRestoration, {}),
       /* @__PURE__ */ jsx(Scripts, {})
     ] })
   ] });
 }
+function App() {
+  return /* @__PURE__ */ jsx(Outlet, {});
+}
 const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
-  default: App
+  Layout,
+  default: App,
+  links
 }, Symbol.toStringTag, { value: "Module" }));
-const serverManifest = { "entry": { "module": "/assets/entry.client-BnXuOxEl.js", "imports": ["/assets/components-Dmit0WD0.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/root-DXxpfufc.js", "imports": ["/assets/components-Dmit0WD0.js"], "css": [] } }, "url": "/assets/manifest-e67693a8.js", "version": "e67693a8" };
+const serverManifest = { "entry": { "module": "/assets/entry.client-CA4eOsHV.js", "imports": ["/assets/components-5fExCxQC.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasErrorBoundary": false, "module": "/assets/root-BAJmJJrD.js", "imports": ["/assets/components-5fExCxQC.js"], "css": ["/assets/root-GcEKw9rS.css"] } }, "url": "/assets/manifest-2e32f977.js", "version": "2e32f977" };
 const mode = "production";
 const assetsBuildDirectory = "build/client";
 const basename = "/";
-const future = { "v3_fetcherPersist": false, "v3_relativeSplatPath": false, "v3_throwAbortReason": false, "v3_routeConfig": false, "v3_singleFetch": false, "v3_lazyRouteDiscovery": false, "unstable_optimizeDeps": false };
+const future = { "v3_fetcherPersist": true, "v3_relativeSplatPath": true, "v3_throwAbortReason": true, "v3_routeConfig": false, "v3_singleFetch": true, "v3_lazyRouteDiscovery": true, "unstable_optimizeDeps": false };
 const isSpaMode = false;
 const publicPath = "/";
 const entry = { module: entryServer };
