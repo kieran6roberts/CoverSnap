ec6bcb9b003bb44221eab84f89c4264b02598572
kieran <kieran6roberts@gmail.com>
2024-12-31 13:18:44 +0000

    refactor: simplify download success modal UI

--- app/components/DownloadSuccessModal.tsx
+++ app/components/DownloadSuccessModal.tsx
@@ -1,33 +1,22 @@
-import { Modal, Stack, Button, Flex, Card, Text, Image, Group, Divider } from '@mantine/core';
+import { Modal, Stack, Button, Flex, Text, Divider, Anchor } from '@mantine/core';
 import { GitHubStarButton } from './GitHubStarButton';
-import { useEditor } from '~/contexts/EditorContext';
 
 export function DownloadSuccessModal({ close }: { close: () => void }) {
-  const { state } = useEditor();
   return (
     <Modal opened onClose={close} centered title="Thanks for using CoverSnap!">
       <Stack>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-              height={160}
-              alt="Norway"
-            />
-          </Card.Section>
-
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>{state.primaryTitle}</Text>
-          </Group>
-        </Card>
         <Divider />
-        <Text>
-          If you have any feedback, please share with me on X{' '}
-          <a target="_blank" href="https://x.com/Kieran6Dev">
+        <Text mb="md">
+          If you have any feedback, please share it with me on X{' '}
+          <Anchor underline="always" target="_blank" href="https://x.com/Kieran6Dev">
             @Kieran6Dev
-          </a>
+          </Anchor>{' '}
+          or raise an issue on{' '}
+          <Anchor underline="always" target="_blank" href="https://github.com/Kieran6Dev/coverSnap/issues">
+            GitHub.
+          </Anchor>
         </Text>
-        <Divider />
+
         <Flex justify="center" align="center" gap="md">
           <GitHubStarButton size="sm" variant="outline" />
           <Button size="sm" variant="filled" onClick={close}>
