2bc4fdc515c043d2077b994540527733c87074e9
kieran <kieran6roberts@gmail.com>
2025-01-02 15:30:09 +0000

    test: another minifed hydration error

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -12,7 +12,8 @@ Cypress.on('uncaught:exception', (err) => {
   if (
     err.message.includes('Hydration') ||
     err.message.includes('hydrating') ||
-    err.message.includes('Minified React error #418')
+    err.message.includes('Minified React error #418') ||
+    err.message.includes('Minified React error #423')
   ) {
     return false;
   }
