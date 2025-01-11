4680a8cae146bebe7827e6286848cb0d38ad19bb
kieran <kieran6roberts@gmail.com>
2025-01-11 09:53:31 +0000

    chore: misc repo generic updates

--- cypress/e2e/download-cover.cy.ts
+++ cypress/e2e/download-cover.cy.ts
@@ -4,17 +4,17 @@ context('Download Cover', () => {
   });
 
   it('Download cover', () => {
-    // User is first greeted with a welcome modal
+    cy.log('User is first greeted with a welcome modal');
     cy.findByRole('heading', { name: 'Welcome to CoverSnap!' }).should('exist');
     cy.findByRole('button', { name: 'Start editing' }).should('exist');
     cy.findByRole('button', { name: 'Start editing' }).click();
     cy.findByRole('heading', { name: 'Welcome to CoverSnap!' }).should('not.exist');
 
-    // Local storage item prevents modal from showing to user again
+    cy.log('Local storage item prevents modal from showing to user again');
     cy.reload();
     cy.findByRole('heading', { name: 'Welcome to CoverSnap!' }).should('not.exist');
 
-    // User can download the cover image
+    cy.log('User can download the cover image');
     cy.findByRole('button', { name: 'Download image' }).should('exist');
     cy.findByRole('button', { name: 'Download image' }).click();
 
