6f01139f21b00d8a07994ebc59e4d1e8fe22b58f
kieran <kieran6roberts@gmail.com>
2025-01-14 13:03:39 +0000

    refactor: text max sizes

--- app/consts/editor.ts
+++ app/consts/editor.ts
@@ -5,9 +5,9 @@ import * as patterns from 'hero-patterns';
 export const PRIMARY_TEXT_LENGTH = 100;
 export const SECONDARY_TEXT_LENGTH = 80;
 export const PRIMARY_TEXT_FONT_SIZE_MIN = 10;
-export const PRIMARY_TEXT_FONT_SIZE_MAX = 80;
+export const PRIMARY_TEXT_FONT_SIZE_MAX = 60;
 export const SECONDARY_TEXT_FONT_SIZE_MIN = 10;
-export const SECONDARY_TEXT_FONT_SIZE_MAX = 60;
+export const SECONDARY_TEXT_FONT_SIZE_MAX = 40;
 
 export const fonts = [
   'Arial',
