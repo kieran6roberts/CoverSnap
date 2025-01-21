bfe2cb63f7c45daa3e533d864eae25e57fd8b0ed
kieran <kieran6roberts@gmail.com>
2025-01-21 20:02:46 +0000

    chore: cleanup deploy specific items + update ci node v

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [netlifyPlugin(), reactRouter(), tsconfigPaths()]
+  plugins: [reactRouter(), tsconfigPaths()]
 });
