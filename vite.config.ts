34f74324e0279968b823fe228765fa8e816cb998
kieran <kieran6roberts@gmail.com>
2025-01-21 17:59:27 +0000

    fix: add cloudflare proxy, fix ci test port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()]
+  plugins: [cloudflareDevProxy(), reactRouter(), tsconfigPaths()]
 });
