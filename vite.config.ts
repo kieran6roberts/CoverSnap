a8a257e49097e7b816141b1b859a80466d214f28
kieran <kieran6roberts@gmail.com>
2025-01-21 19:19:28 +0000

    fix: add netlify adapters + e2e config port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()]
+  plugins: [netlifyPlugin(), reactRouter(), tsconfigPaths()]
 });
