eb69ed7c8b6a0007e4313c3d78db3758ed4a60ae
kieran <kieran6roberts@gmail.com>
2025-01-22 19:58:11 +0000

    feat: add robots/sitemap

--- packages/frontend/app/routes.ts
+++ packages/frontend/app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, route, index } from '@react-router/dev/routes';
 
-export default [index('./routes/index.tsx'), route('/create', './routes/create/route.tsx')] satisfies RouteConfig;
+export default [
+ index('./routes/index.tsx'), 
+ route('/create', './routes/create/route.tsx'), 
+ route('/sitemap.xml', './routes/sitemap.xml.tsx')
+] satisfies RouteConfig;
